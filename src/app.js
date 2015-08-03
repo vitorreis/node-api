2f8d5e791148e8ec460d1dd079ebcca4e1a527ea
Vitor Reis <vitor.reis@rocket-internet.com.br>
2015-08-02 22:20:30 -0300

    hey! created from the scratch

---
+++ src/app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+    res.send('Hello from book api');
+});
+
+app.listen(port, function(){
+    console.log('running on PORT: ', port);
+});
