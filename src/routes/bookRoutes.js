3e139d84f400dfde9237e0c183d4aa1719e203c9
Vitor Reis <vitor.reis@rocket-internet.com.br>
2015-08-05 11:08:24 -0300

    added PATCH and DELETE methods

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -27,15 +27,15 @@ var routes = function (Book) {
 //midleware
 bookRouter.use('/:bookId', function(req, res, next){
     Book.findById(req.params.bookId, function(err, book){
-            if(err)
-                res.status(500).send(err);
-            else if (book){
-                req.body = book;
-                next();
-            }
-            else
-                res.status(404).send('no book found');
-        });
+        if(err)
+            res.status(500).send(err);
+        else if (book){
+            req.book = book;
+            next();
+        }
+        else
+            res.status(404).send('no book found');
+    });
 })
 
 bookRouter.route('/:bookId')
@@ -48,8 +48,38 @@ bookRouter.route('/:bookId')
         req.book.author = req.body.author;
         req.book.read = req.body.read;
 
-        req.book.save();
+        req.book.save(function(err){
+            if (err)
+                res.status(500).send(err);
+            else
+                res.json(req.book);
+
+        });
         res.json(req.book);
+    })
+    .patch(function(req, res){
+        if (req.body._id)
+            delete req.body._id;
+
+        for(var p in req.body){
+            req.book[p] = req.body[p];
+        }
+
+        req.book.save(function(err){
+            if (err)
+                res.status(500).send(err);
+            else
+                res.json(req.book);
+        });
+
+    })
+    .delete(function(req, res){
+        req.book.remove(function(err){
+            if (err)
+                res.status(500).send(err);
+            else
+                res.status(204).send('removed');
+        });
     });
 
     return bookRouter;
