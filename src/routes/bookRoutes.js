02e2a19b4fe7a62f6c2bc42c990fae94aee61296
Vitor Reis <vitor.reis@rocket-internet.com.br>
2015-08-05 13:31:18 -0300

    added HATEOAS support

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -10,63 +10,70 @@ var routes = function (Book) {
         .get(bookController.get)
         .post(bookController.post);
 
-//midleware
-bookRouter.use('/:bookId', function(req, res, next){
-    Book.findById(req.params.bookId, function(err, book){
-        if(err)
-            res.status(500).send(err);
-        else if (book){
-            req.book = book;
-            next();
-        }
-        else
-            res.status(404).send('no book found');
-    });
-})
-
-bookRouter.route('/:bookId')
-    .get(function(req, res){
-        res.json(req.book);
-    })
-    .put(function(req, res){
-        req.book.title = req.body.title;
-        req.book.genre = req.body.genre;
-        req.book.author = req.body.author;
-        req.book.read = req.body.read;
-
-        req.book.save(function(err){
-            if (err)
+    //midleware
+    bookRouter.use('/:bookId', function(req, res, next){
+        Book.findById(req.params.bookId, function(err, book){
+            if(err)
                 res.status(500).send(err);
+            else if (book){
+                req.book = book;
+                next();
+            }
             else
-                res.json(req.book);
-
+                res.status(404).send('no book found');
         });
-        res.json(req.book);
     })
-    .patch(function(req, res){
-        if (req.body._id)
-            delete req.body._id;
 
-        for(var p in req.body){
-            req.book[p] = req.body[p];
-        }
+    bookRouter.route('/:bookId')
+        .get(function(req, res){
 
-        req.book.save(function(err){
-            if (err)
-                res.status(500).send(err);
-            else
-                res.json(req.book);
+            var returnBook = req.book.toJSON();
+
+            returnBook.links = {};
+            var newLink = 'http://' + req.headers.host + '/api/books/?genre=' + returnBook.genre;
+            returnBook.links.FilterByGenre = encodeURIComponent(newLink);
+
+            res.json(returnBook);
+        })
+        .put(function(req, res){
+            req.book.title = req.body.title;
+            req.book.genre = req.body.genre;
+            req.book.author = req.body.author;
+            req.book.read = req.body.read;
+
+            req.book.save(function(err){
+                if (err)
+                    res.status(500).send(err);
+                else
+                    res.json(req.book);
+
+            });
+            res.json(req.book);
+        })
+        .patch(function(req, res){
+            if (req.body._id)
+                delete req.body._id;
+
+            for(var p in req.body){
+                req.book[p] = req.body[p];
+            }
+
+            req.book.save(function(err){
+                if (err)
+                    res.status(500).send(err);
+                else
+                    res.json(req.book);
+            });
+
+        })
+        .delete(function(req, res){
+            req.book.remove(function(err){
+                if (err)
+                    res.status(500).send(err);
+                else
+                    res.status(204).send('removed');
+            });
         });
-
-    })
-    .delete(function(req, res){
-        req.book.remove(function(err){
-            if (err)
-                res.status(500).send(err);
-            else
-                res.status(204).send('removed');
-        });
-    });
 
     return bookRouter;
 
